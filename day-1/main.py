ecbeccc2fc49e61a1f319d4089657817805b281a
ashikkhulal <askhulal@gmail.com>
2022-11-10 22:24:53 -0600

    added day 1 materials

---
+++ day-1/main.py
@@ -0,0 +1,53 @@
+## Print Function
+# Prints a string into the console.  
+# Example: 
+# print("Hello World")
+
+# Print Function - Assignment Answer:
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+## Input Function
+# Prints a string into the console,and asks the user for a string input.
+# Example: 
+# input("What's your name")
+
+# Input Function - Assignment Answer:
+a = input("What is your name? ")
+b = len(a)
+print(b) 
+
+## Comments
+# Adding a # symbol in font of textlets you make comments on a line of code.The computer will ignore your comments.
+# Example: 
+# #This is a commentprint("This is code")
+
+## Variables
+# A variable give a name to a piece of data.Like a box with a label, it tells you what's inside the box. 
+# Example: 
+# my_name = "Angela"my_age = 12
+
+# Variables - Assignment Answer:
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+c = a
+a = b
+b = c
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+
+
+
---
+++ day-1/project/band-name-generator.py
@@ -0,0 +1,13 @@
+#1. Create a greeting for your program.
+print(f"Hello,  Welcome to band name generator!\nAnswer the below two questions to generate a name for your music band.")
+
+#2. Ask the user for the city that they grew up in.
+city = input(f"What city did you grew up in?\n")
+
+#3. Ask the user for the name of a pet.
+petName = input(f"What is the name of your pet?\n")
+
+#4. Combine the name of their city and pet and show them their band name.
+print(f"The name of your music band is: {city} {petName}.")
+
+#5. Make sure the input cursor shows on a new line:
